ee151af69b715bfb0d601f7295b732853b80d877
san2codes <santu5307@gmail.com>
2026-06-22 16:05:09 +0530

    production api url

--- frontend/src/Api/axios.js
+++ frontend/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://travel-planner-backend-now0.onrender.com/api",
 });
 
 export default api;
